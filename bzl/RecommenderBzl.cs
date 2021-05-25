05a7a0d1f87df2139e57d90801f1b22f0d8cef65
Eduard Laitin <laitineduard@gmail.com>
2021-05-25 16:00:17 +0300

    fix title mock

--- bzl/RecommenderBzl.cs
+++ bzl/RecommenderBzl.cs
@@ -16,7 +16,7 @@ namespace FilmrecAPI.bzl
                 averageRating = 8.5,
                 imageSource = "https://image.tmdb.org/t/p/w500/h5J4W4veyxMXDMjeNxZI46TsHOb.jpg",
                 tagline = "Life is like a box of chocolates...you never know what you're gonna get.",
-                title = "TITLE"
+                title = "Forrest Gump"
             };
             return Task.FromResult(result);
         }
